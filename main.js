1abfea0487a5df2ec8070cc373df188ed6729d2c
Dmytro Khomenko <dmytro081102@gmail.com>
2023-01-11 22:37:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let name="dmytro";
+let admin=name;
+console.log(admin);
+let days=5;
+console.log(days*24*60*60);
+let promptValue = prompt("Введіть якесь значення", "");
+console.log(promptValue);
